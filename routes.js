69c24bfe841d9779539c7c7a5dfec0e7ac2e58c8
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-06 16:23:20 -0600

    Update routes.js
    Typed in code for each route to follow when called

---
+++ routes.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const { sequelize, models } = require('./models');
+const { User, Course} = models;
+
+function asyncHandler(cb) {
+    return async (req, res, next) => {
+        try{
+            await cb(req, res, next);
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+router.get('/courses', asyncHandler( async(req, res) => {
+    let courses = await Course.findAll();
+    res.status(200).json(courses);
+}));
+
+router.get('/courses:id', asyncHandler( async(req, res) => {
+    const course = await Course.getCourse(req.params.id);
+
+    if (course) {
+        res.status(200),json(course)
+    } else {
+        res.status(404).json({ message: "Course Not Found" });
+    }
+}));
+
+router.post('/courses', asyncHandler( async(req, res) => {
+    try {
+        await Course.create(req.body);
+        res.status(201).json({ message: "Course created successfully!" });
+    } catch (error) {
+        console.log('ERROR: ', error.name);
+
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        }
+    }
+}));
+
+router.put('/courses:id', asyncHandler( async(req, res) => {
+    const course = await Course.getCourse(req.params.id);
+    if (course) {
+        course.title = req.body.title;
+        course.description = req.body.description;
+        course.estimatedTime = req.body.estimatedTime;
+        course.materialsNeeded = req.body.materialsNeeded;
+
+        await Course.updateCourse(course);
+        res.status(204).end();
+    } else {
+        res.status(404).json({ message: "Course Not Found" });
+    }
+}));
+
+router.delete('/courses:id', asyncHandler( async(req, res) => {
+    const course = await Course.getCourse(req.params.id);
+    await Course.deleteCourse(course);
+    res.status(204).end();
+}));
+
+module.exports = router;
