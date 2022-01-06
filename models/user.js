9df2a7ca7d48b295bc4b13015cf61bc5d27e4e94
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-06 17:33:59 -0600

    Update user.js
    removing /dist and adding validation parameters to User

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { Model, DataTypes } = require("sequelize/dist");
+const { Model, DataTypes } = require("sequelize");
 const bcrypt = require('bcrypt');
 const sequelize = require("sequelize");
 
@@ -9,16 +9,75 @@ module.exports = (sequelize) => {
     User.init({
         firstName: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A first name is required.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a first name'
+                },
+            }
         },
         lastName: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A last name is required.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a last name'
+                },
+            }
+            
         },
         emailAddress: {
             type: DataTypes.STRING,
+            allowNull: false,
+            unique: {
+                msg: 'The email you entered is already being used.'
+            },
+            validate: {
+                notNull: {
+                    msg: 'An email is required.'
+                },
+                isEmail: {
+                    msg: 'Please provide a valid email address.'
+                }
+            }
         },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
+            allowNull: false,
+            validate: {
+                notNull: {
+                    msg: 'A password is required.'
+                },
+                notEmpty: {
+                    msg: 'Please provide a password.'
+                },
+                len: {
+                    args: [8, 20],
+                    msg: 'The password should be between 8 and 20 characters in length.'
+                }
+            }
         },
+        confirmedPassword: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            set(val) {
+              if ( val=== this.password){
+                const hashedPassword = bcrypt.hashSync(val, 10);
+                this.setDataValue('confirmedPassword', hashedPassword);
+              }
+            },
+            validate: {
+              notNull: {
+                msg: 'Both passwords must match'
+              }
+            },
+          }
     }, { sequelize });
 
     User.associate = (models) => {
