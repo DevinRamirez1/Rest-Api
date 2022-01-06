984c04787c30a57e7d11befaea5744c963cc5006
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-06 17:49:52 -0600

    Update auth-user.js
    Code to authenticate user login info when entered

---
+++ middleware/auth-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const auth = require('basic-auth');
+const { User } = require('../models');
+const bcrypt = require('bcrypt');
+
+//Middleware to authenticate the request
+exports.authenticateUser = async( req, res, next) => {
+    let message;
+
+    const credentials = auth(req);
+
+    if (credentials) {
+        const user = await User.findOne({ where: {emailAddress: credentials.name}});
+        if (user) {
+            const authenticated = bcrypt.compareSync(credentials.pass, user.confirmedPassword);
+            if (authenticated) {
+                console.log(`Authentication was successful for email address: ${user.emailAddress}`);
+                req.currentUser = user;
+            } else {
+                message = `Authentication failed for email address: ${user.emailAddress}`;
+            }
+        } else {
+            message = `User not found with following email address: ${user.emailAddress}`;
+        }
+    } else {
+        message = `Auth header not found`;
+    }
+
+    if (message) {
+        console.warn(message);
+        res.status(401).json({ message: 'Access Denied' });
+    } else {
+    next();
+    }
+};
