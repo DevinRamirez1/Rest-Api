d40481263fda5215eb3905a53b7676b2083ceb84
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-07 16:53:41 -0600

    Update routes.js
    Adding authentication to different routes

--- routes.js
+++ routes.js
@@ -39,12 +39,28 @@ router.post('/users', asyncHandler( async(req, res) => {
 }));
 
 router.get('/courses', asyncHandler( async(req, res) => {
-    let courses = await Course.findAll();
+    const courses = await Course.findAll({
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: [
+            {
+                model: User,
+                attributes: ['firstName', 'lastName', 'emailAddress'],
+            }
+        ]
+    });
     res.status(200).json(courses);
 }));
 
 router.get('/courses:id', asyncHandler( async(req, res) => {
-    const course = await Course.getCourse(req.params.id);
+    const course = await Course.getCourse(req.params.id, {
+        attributes: { exclude: ["createdAt", "updatedAt"] },
+        include: [
+            {
+                model: User,
+                attributes: ['firstName', 'lastName', 'emailAddress'],
+            }
+        ]
+    });
 
     if (course) {
         res.status(200),json(course)
@@ -53,7 +69,7 @@ router.get('/courses:id', asyncHandler( async(req, res) => {
     }
 }));
 
-router.post('/courses', asyncHandler( async(req, res) => {
+router.post('/courses', authenticateUser, asyncHandler( async(req, res) => {
     try {
         await Course.create(req.body);
         res.status(201).json({ message: "Course created successfully!" });
@@ -69,25 +85,37 @@ router.post('/courses', asyncHandler( async(req, res) => {
     }
 }));
 
-router.put('/courses:id', asyncHandler( async(req, res) => {
-    const course = await Course.getCourse(req.params.id);
-    if (course) {
-        course.title = req.body.title;
-        course.description = req.body.description;
-        course.estimatedTime = req.body.estimatedTime;
-        course.materialsNeeded = req.body.materialsNeeded;
-
-        await Course.updateCourse(course);
-        res.status(204).end();
-    } else {
-        res.status(404).json({ message: "Course Not Found" });
+router.put('/courses:id', authenticateUser, asyncHandler( async(req, res) => {
+    try{
+        const user = req.currentUser;
+        const course = await Course.getCourse(req.params.id);
+        if (course && course.userId === user.id) {
+            await course.update(req.body);
+            res.status(204).end();
+        } else {
+            res.status(403).json({ message: "Looks like you do not own this course. Please try again." });
+            next(err);
+        }
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });
+        } else {
+            throw error;
+        } 
     }
 }));
 
-router.delete('/courses:id', asyncHandler( async(req, res) => {
+router.delete('/courses:id', authenticateUser, asyncHandler( async(req, res) => {
+    const user = req.currentUser;
     const course = await Course.getCourse(req.params.id);
-    await Course.deleteCourse(course);
-    res.status(204).end();
+        if (course && course.userId === user.id) {
+            await Course.deleteCourse(course);
+            res.status(204).end();
+        } else {
+            const err = createError(403, "Looks like you do not own this course. Please try again.");
+            next(err);
+        }
 }));
 
 module.exports = router;
