1e714b517c105c769eb4b74ed67ef49e854cd8da
DevinRamirez1 <devinramirez207@gmail.com>
2022-01-10 16:28:42 -0600

    Update routes.js
    Adding location to the post and end

--- routes.js
+++ routes.js
@@ -30,7 +30,7 @@ router.get('/users', authenticateUser, asyncHandler( async(req, res) => {
 router.post('/users', asyncHandler( async(req, res) => {
     try {
         await User.create(req.body);
-        res.status(201).json();
+        res.status(201).location("/").end();
     } catch (error) {
         console.log('ERROR: ', error.name);
 
